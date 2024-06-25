6d35d0118a59e3159a30d676ba0c6937304fa06b
myerspat <myerspat@umich.edu>
2024-06-25 13:26:39 -0400

    updated regression test

--- tests/regression/test_crit_verif.py
+++ tests/regression/test_crit_verif.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 import tt_nte.benchmarks as benchmarks
-from tt_nte.methods import DiscreteOrdinates
 import tt_nte.solvers as solvers
+from tt_nte.methods import DiscreteOrdinates
 
 
 def test_single_media():
@@ -24,9 +24,9 @@ def test_single_media():
     )
 
     # Assertions
-    assert SN.H.matricize().shape == (1024 * 2, 1024 * 3)
-    assert SN.S.matricize().shape == (1024 * 2, 1024 * 3)
-    assert SN.F.matricize().shape == (1024 * 2, 1024 * 3)
+    assert SN.H.matricize().shape == (1024 * 2, 1024 * 2)
+    assert SN.S.matricize().shape == (1024 * 2, 1024 * 2)
+    assert SN.F.matricize().shape == (1024 * 2, 1024 * 2)
 
     # Ordinates and solvers to test
     num_ordinates = [2, 8]
@@ -34,6 +34,7 @@ def test_single_media():
 
     # Expected solutions
     expected_k = [0.80418, 0.99176]
+    expected_psi = []
 
     for i in range(len(num_ordinates)):
         # Change number of ordinates in SN
@@ -48,7 +49,7 @@ def test_single_media():
         assert solver.psi.shape == (1024 * num_ordinates[i],)
 
         # Save psi
-        expected_psi = solver.psi
+        expected_psi.append(solver.psi)
 
         for solver in tt_solvers:
             # Initialize solver
@@ -59,75 +60,48 @@ def test_single_media():
 
             # Assertions
             assert abs(expected_k[i] - solver.k) < 0.00005
-            assert solver.psi.shape == (1024 * num_ordinates[i],)
+            assert (
+                solver.psi.shape
+                if solver.__class__.__name__ == "Matrix"
+                else solver.psi.matricize().shape == (1024 * num_ordinates[i],)
+            )
             assert (
                 np.linalg.norm(
-                    expected_psi - solver.psi
+                    expected_psi[i] - solver.psi
                     if solver.__class__.__name__ == "Matrix"
-                    else solver.psi.matricize()
+                    else expected_psi[i] - solver.psi.matricize()
                 )
-                < 0.02
+                < 0.002
             )
 
     # ----------------------------------------------------------------
     # SN in QTT format
     qtt_solvers = [solvers.MALS, solvers.GMRES]
+    qtt_solver_configs = [{"max_rank": 20}, {}]
 
     for i in range(len(num_ordinates)):
         # Change number of ordinates in SN
         SN.update_settings(num_ordinates=num_ordinates[i], tt_fmt="qtt")
 
-        # Run matrix generalized eigenvalue solver
-        solver = solvers.Matrix(method=SN, verbose=True)
-        solver.ges()
-
-        # Assertions
-        assert abs(expected_k[i] - solver.k) < 0.00005
-        assert solver.psi.shape == (1024 * num_ordinates[i],)
-
-        # Save psi
-        expected_psi = solver.psi
-
-        for solver in qtt_solvers:
+        for j in range(len(qtt_solvers)):
             # Initialize solver
-            solver = solver(method=SN, verbose=True)
+            solver = qtt_solvers[j](method=SN, verbose=True, **qtt_solver_configs[j])
 
             # Run power iteration
-            solver.power()
+            solver.power(ranks=4)
 
             # Assertions
             assert abs(expected_k[i] - solver.k) < 0.00005
-            assert solver.psi.shape == (1024 * num_ordinates[i],)
+            assert (
+                solver.psi.shape
+                if solver.__class__.__name__ == "Matrix"
+                else solver.psi.matricize().shape == (1024 * num_ordinates[i],)
+            )
             assert (
                 np.linalg.norm(
-                    expected_psi - solver.psi
+                    expected_psi[i] - solver.psi
                     if solver.__class__.__name__ == "Matrix"
-                    else solver.psi.matricize()
+                    else expected_psi[i] - solver.psi.matricize()
                 )
-                < 0.02
+                < 0.002
             )
-
-
-# def test_two_media():
-#     # Set numpy random seed
-#     np.random.seed(42)
-#
-#     # Get two-media problem geometry and XS server
-#     server, geometry = benchmarks.research_reactor_multi_region(
-#         [132, 760, 132], "vacuum"
-#     )
-#
-#     # Initialize SN solver
-#     SN = DiscreteOrdinates(
-#         xs_server=server,
-#         geometry=geometry,
-#         num_ordinates=32,
-#         tt_fmt="qtt",
-#         qtt_threshold=1e-15,
-#     )
-#
-#     # Initialize and run solver
-#     solver = solvers.AMEn(SN, verbose=True)
-#     solver.power(tol=1e-5, max_iter=2000)
-#
-#     assert abs(solver.k - 0.9993) < 0.0005
